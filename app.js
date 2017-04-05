a05d2b687fca7c803e5b5234afa1cba1a273ae77
Luke Schuyler <lukeschuyler1@gmail.com>
2017-04-04 21:19:57 -0500

    scraper

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const leeka = {
-  myNear: () => {
-    console.log('leeka')
-    setTimeout(() => {
-      console.log('myNear')
-    }, 3000)
-  }
-}
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
 
-const { myNear } = leeka
+app.get('/', function(req, res){
+  request('http://www.zeldadungeon.net/ocarina-of-time-walkthrough/fire-temple/', function (error, response, html) {
+        var $ = cheerio.load(html);
+        var walkthrough;
+        var json = { walkthrough : "", release : "", rating : ""};
+     if(!error){
+        $('div.walkthrough p').filter(function(){
+        var data = $(this);
+        walkthrough = data.text();
+        console.log(walkthrough)
+      })
+    }
+  })
+})
 
-myNear()
+app.listen('8080');
+module.exports.app = app;
+
+// run nodemon app.js then look at terminal console
