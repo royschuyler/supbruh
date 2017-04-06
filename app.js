3dbe7c3a468a4270bb87c5dac8a48095ac9682cd
Luke Schuyler <lukeschuyler1@gmail.com>
2017-04-05 19:46:43 -0500

    removed get, can now enter args for search

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
+let game,
+    dungeon,
+    gameString,
+    dungeonString;
+let [,,...args] = process.argv;
+let dash = args.indexOf('-')
+let postDash = args.splice(dash + 1)
 
-app.get('/', function(req, res){
-  request('http://www.zeldadungeon.net/ocarina-of-time-walkthrough/fire-temple/', function (error, response, html) {
-        var $ = cheerio.load(html);
+for (let j = 0; j < dash; j++) {
+  gameString += args[j] + '-'
+}
+  game = gameString.slice(0, -1).slice(9)
+
+for (let i = 0; i < postDash.length; i++) {
+  dungeonString += postDash[i] + '-'
+} 
+  dungeon = dungeonString.slice(0, -1).slice(9)
+
+
+// app.get('/', function(req, res){
+  request(`http://www.zeldadungeon.net/${game}-walkthrough/${dungeon}/`, function (error, response, html) {
         var walkthrough;
+        var $ = cheerio.load(html);
         var json = { walkthrough : "", release : "", rating : ""};
      if(!error){
         $('div.walkthrough p').filter(function(){
@@ -17,9 +35,10 @@ app.get('/', function(req, res){
       })
     }
   })
-})
+// })
 
 app.listen('8080');
 module.exports.app = app;
+
 
 // run nodemon app.js then look at terminal console
